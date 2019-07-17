cbe8b3a61e78cac12a9edc5d0be099a82ac78527
Yikai Zhang <zhangyikai@hotmail.com>
2019-07-17 17:47:54 -0400

    add local store page

--- src/components/LocalStorePage.js
+++ src/components/LocalStorePage.js
@@ -5,8 +5,32 @@ class LocalStorePage extends Component {
     return (
       <div className="local-store-page transition-item">
         <div className="LocalStore">
-          <h1 className="LocalStore__text">To be continued</h1>
-          <img src="/assets/images/to-be-continued.png" alt="find-in-store" />
+          <div className="LocalStore__info">
+            <h3>Textile Museum of Canada</h3>
+            <h4>For Exclusive Collection</h4>
+            <div>
+              <p>55 Centre Avenue, Toronto, Ontario, M5G 2H5</p>
+              <p>(416) 599-5321</p>
+              <p>info@textilemuseum.ca</p>
+            </div>
+          </div>
+          <div className="LocalStore__info">
+            <h3>Craft Ontario</h3>
+            <h4>For Full Collection</h4>
+            <div>
+              <p>1106 Queen St. West, Toronto, Ontario, M6J 1H9</p>
+              <p>(416) 921-1721</p>
+              <p>shop@craftontario.com</p>
+            </div>
+          </div>
+          <div className="LocalStore__info">
+            <h3>KaKaFaFa Ideas</h3>
+            <h4>For Girls Collection</h4>
+            <div>
+              <p>275 Dundas Street West, Toronto, ON M5T 3K1</p>
+              <p>(647) 348-2880</p>
+            </div>
+          </div>
         </div>
       </div>
     );
