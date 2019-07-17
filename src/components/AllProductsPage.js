18d41fe350ea88daccd598a9ae6cd2a0debf7fef
Yikai Zhang <zhangyikai@hotmail.com>
2019-07-17 16:48:59 -0400

    fix thumbnail issue in allProductsPage

--- src/components/AllProductsPage.js
+++ src/components/AllProductsPage.js
@@ -35,11 +35,21 @@ class AllProducts extends Component {
               {availableProducts.map(product => (
                 <div key={product.id}>
                   <Link to={`/work/${product.handle}`}>
-                    <img
-                      src={product.images[0].src}
-                      alt="scarf"
-                      className="AvailableProducts__image"
-                    />
+                    {product.options[2].values[0].value === "Square" ? (
+                      <img
+                        src={product.images[0].src}
+                        draggable="false"
+                        alt="scarf"
+                        className="AvailableProducts__image"
+                      />
+                    ) : (
+                      <img
+                        src={product.images[2].src}
+                        draggable="false"
+                        alt="scarf"
+                        className="AvailableProducts__image"
+                      />
+                    )}
                   </Link>
                   <Link to={`/work/${product.handle}`}>
                     <p className="AvailableProducts__name">{product.title}</p>
