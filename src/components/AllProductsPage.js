b781c3f8ce5ed419004b18f20f58fbce9190995e
Yikai Zhang <zhangyikai@hotmail.com>
2019-08-16 12:18:57 -0400

    fix the position issue for the available products page when click the back button from the specific product page and also update the css style

--- src/components/AllProductsPage.js
+++ src/components/AllProductsPage.js
@@ -5,136 +5,139 @@ import { BrowserView, MobileView, isBrowser } from "react-device-detect";
 import Smooth from "../utils/Smooth";
 
 class AllProducts extends Component {
-  componentDidMount() {
-    if (isBrowser) {
-      document.body.style.cssText = "";
-      new Smooth();
-    }
-  }
+	componentDidMount() {
+		if (isBrowser) {
+			document.body.style.cssText = "";
+			new Smooth();
+		}
+	}
 
-  componentDidUpdate(prevProps) {
-    if (isBrowser) {
-      //Make sure the component will get properly re-rendered even if the props get updated after the componentDidMount call
-      if (this.props.availableProducts !== prevProps.availableProducts) {
-        document.body.style.cssText = "";
-        new Smooth();
-      }
-    }
-  }
+	componentDidUpdate(prevProps) {
+		if (isBrowser) {
+			//Make sure the component will get properly re-rendered even if the props get updated after the componentDidMount call
+			if (this.props.availableProducts !== prevProps.availableProducts) {
+				document.body.style.cssText = "";
+				new Smooth();
+			}
+		}
+	}
 
-  componentWillUnmount() {}
+	componentWillUnmount() {}
 
-  render() {
-    const { availableProducts } = this.props;
-    return (
-      <div className="allProducts-page transition-item" data-scroll>
-        <MobileView>
-          <div className="AvailableProducts">
-            <h1 className="AvailableProducts__title">All Products</h1>
-            <div className="AvailableProducts__grid">
-              {availableProducts.map(product => (
-                <div key={product.id}>
-                  <Link to={`/work/${product.handle}`}>
-                    {product.options[2].values[0].value === "Square" ? (
-                      <img
-                        src={product.images[0].src}
-                        draggable="false"
-                        alt="scarf"
-                        className="AvailableProducts__image"
-                      />
-                    ) : (
-                      <img
-                        src={product.images[2].src}
-                        draggable="false"
-                        alt="scarf"
-                        className="AvailableProducts__image"
-                      />
-                    )}
-                  </Link>
-                  <Link to={`/work/${product.handle}`}>
-                    <p className="AvailableProducts__name">{product.title}</p>
-                  </Link>
-                </div>
-              ))}
-            </div>
-          </div>
-        </MobileView>
-        <BrowserView>
-          <div className="AvailableProducts--update">
-            <div className="scroll-content" data-scroll-content>
-              {availableProducts.map((product, index) => (
-                <article
-                  className={`slide slide--${index} js-slide`}
-                  key={product.id}
-                >
-                  <div className="slide__inner">
-                    <div className="slide__img js-transition-img">
-                      <figure className="js-transition-img__inner">
-                        {product.options[2].values[0].value === "Square" ? (
-                          <img
-                            src={product.images[0].src}
-                            draggable="false"
-                            alt="scarf"
-                          />
-                        ) : (
-                          <img
-                            src={product.images[2].src}
-                            draggable="false"
-                            alt="scarf"
-                          />
-                        )}
-                      </figure>
-                    </div>
-                  </div>
-                </article>
-              ))}
-            </div>
-            <div
-              className="scroll-content scroll-content--last"
-              data-scroll-content
-            >
-              {availableProducts.map((product, index) => (
-                <article
-                  className={`slide slide--${index} js-slide`}
-                  key={product.id}
-                >
-                  <div className="slide__inner">
-                    <div className="slide__sub-title">
-                      <span>{product.options[0].values[0].value}</span>
-                    </div>
-                    <h1 className="slide__title">
-                      <div className="js-transition-title">{product.title}</div>
-                    </h1>
-                    <div className="slide__img slide__img--proxy">
-                      <Link
-                        className="slide__img-link"
-                        to={`/work/${product.handle}`}
-                      />
-                    </div>
-                    <div className="slide__project">
-                      {product.options[1].values[0].value}
-                    </div>
-                  </div>
-                </article>
-              ))}
-            </div>
-            <div className="scrollbar" data-scrollbar>
-              <div className="scrollbar__handle js-scrollbar__handle" />
-            </div>
-          </div>
-        </BrowserView>
-      </div>
-    );
-  }
+	render() {
+		const { availableProducts } = this.props;
+		return (
+			<div className="allProducts-page transition-item" data-scroll>
+				<MobileView>
+					<div className="AvailableProducts">
+						<h1 className="AvailableProducts__title">All Products</h1>
+						<div className="AvailableProducts__grid">
+							{availableProducts.map(product => (
+								<div key={product.id}>
+									<Link to={`/work/${product.handle}`}>
+										{product.options[2].values[0].value === "Square" ? (
+											<img
+												src={product.images[0].src}
+												draggable="false"
+												alt="scarf"
+												className="AvailableProducts__image"
+											/>
+										) : (
+											<img
+												src={product.images[2].src}
+												draggable="false"
+												alt="scarf"
+												className="AvailableProducts__image"
+											/>
+										)}
+									</Link>
+									<Link to={`/work/${product.handle}`}>
+										<p className="AvailableProducts__name">{product.title}</p>
+									</Link>
+								</div>
+							))}
+						</div>
+					</div>
+				</MobileView>
+				<BrowserView>
+					<div className="AvailableProducts--update">
+						<div className="scroll-content" data-scroll-content>
+							{availableProducts.map((product, index) => (
+								<article
+									className={`slide slide--${index} js-slide`}
+									key={product.id}
+								>
+									<div className="slide__inner">
+										<div className="slide__img js-transition-img">
+											<figure className="js-transition-img__inner">
+												{product.options[2].values[0].value === "Square" ? (
+													<img
+														src={product.images[0].src}
+														draggable="false"
+														alt="scarf"
+													/>
+												) : (
+													<img
+														src={product.images[2].src}
+														draggable="false"
+														alt="scarf"
+													/>
+												)}
+											</figure>
+										</div>
+									</div>
+								</article>
+							))}
+						</div>
+						<div
+							className="scroll-content scroll-content--last"
+							data-scroll-content
+						>
+							{availableProducts.map((product, index) => (
+								<article
+									className={`slide slide--${index} js-slide`}
+									key={product.id}
+								>
+									<div className="slide__inner">
+										<div className="slide__sub-title__top">
+											<span>{product.options[0].values[0].value}</span>
+										</div>
+										<h1 className="slide__title">
+											<div className="js-transition-title">{product.title}</div>
+										</h1>
+										<div className="slide__sub-title__bottom">
+											<span>{product.options[2].values[0].value}</span>
+										</div>
+										<div className="slide__img slide__img--proxy">
+											<Link
+												className="slide__img-link"
+												to={`/work/${product.handle}`}
+											/>
+										</div>
+										<div className="slide__project">
+											{product.options[1].values[0].value}
+										</div>
+									</div>
+								</article>
+							))}
+						</div>
+						<div className="scrollbar" data-scrollbar>
+							<div className="scrollbar__handle js-scrollbar__handle" />
+						</div>
+					</div>
+				</BrowserView>
+			</div>
+		);
+	}
 }
 
 function mapStateToProps(state) {
-  return {
-    availableProducts: state.availableProducts
-  };
+	return {
+		availableProducts: state.availableProducts
+	};
 }
 
 export default connect(
-  mapStateToProps,
-  null
+	mapStateToProps,
+	null
 )(AllProducts);
--- src/components/ProductPage.js
+++ src/components/ProductPage.js
@@ -4,10 +4,10 @@ import { isMobile, isBrowser } from "react-device-detect";
 import { Link } from "react-router-dom";
 import { withRouter } from "react-router";
 import {
-  loadProduct,
-  unloadProduct,
-  addToCart,
-  toggleCart
+	loadProduct,
+	unloadProduct,
+	addToCart,
+	toggleCart
 } from "../actions/actions";
 import TextureDisplacement from "./TextureDisplacement";
 import Charming from "react-charming";
@@ -15,154 +15,154 @@ import ProductOverlay from "./ProductOverlay";
 import SmoothScroll from "../utils/SmoothScroll";
 
 class ProductPage extends Component {
-  componentDidMount() {
-    this.props.loadProduct(this.props.match.params.handle);
-    document.body.style.cssText = "";
-    if (isBrowser) {
-      new SmoothScroll();
-    }
-  }
+	componentDidMount() {
+		this.props.loadProduct(this.props.match.params.handle);
+		document.body.style.cssText = "";
+		if (isBrowser) {
+			new SmoothScroll();
+		}
+	}
 
-  componentDidUpdate(prevProps) {
-    if (this.props.product !== prevProps.product) {
-      document.body.style.cssText = "";
-      if (isBrowser) {
-        new SmoothScroll();
-      }
-    }
-  }
+	componentDidUpdate(prevProps) {
+		if (this.props.product !== prevProps.product) {
+			document.body.style.cssText = "";
+			if (isBrowser) {
+				new SmoothScroll();
+			}
+		}
+	}
 
-  componentWillUnmount() {
-    this.props.unloadProduct();
-  }
+	componentWillUnmount() {
+		this.props.unloadProduct();
+	}
 
-  render() {
-    const { product, checkout } = this.props;
-    let productPrice;
-    let productMarkup;
+	render() {
+		const { product, checkout } = this.props;
+		let productPrice;
+		let productMarkup;
 
-    if (product.hasOwnProperty("images")) {
-      productPrice = product.variants[0].price;
-      productMarkup = { __html: product.descriptionHtml };
-    }
-    return (
-      <div className="product-page smooth-scroll-wrapper">
-        <div className="product-page-content smooth-scroll-content">
-          {product.hasOwnProperty("images") ? (
-            <div className="Product-hero">
-              <div>
-                <Charming
-                  letters={product.title}
-                  render={letters => (
-                    <h1 className="Product-hero__title charming">{letters}</h1>
-                  )}
-                />
-                {/* <h1 className="Product-hero__title">{product.title}</h1> */}
-                <div className="Product-hero__image">
-                  {isMobile ? (
-                    <img src={product.images[0].src} alt="product" />
-                  ) : (
-                    <div>
-                      <ProductOverlay product={product} />
-                      <TextureDisplacement
-                        image={product.images[0].src}
-                        handle={product.handle}
-                        size="default"
-                      />
-                    </div>
-                  )}
+		if (product.hasOwnProperty("images")) {
+			productPrice = product.variants[0].price;
+			productMarkup = { __html: product.descriptionHtml };
+		}
+		return (
+			<div className="product-page smooth-scroll-wrapper">
+				<div className="product-page-content smooth-scroll-content">
+					{product.hasOwnProperty("images") ? (
+						<div className="Product-hero">
+							<div>
+								<Charming
+									letters={product.title}
+									render={letters => (
+										<h1 className="Product-hero__title charming">{letters}</h1>
+									)}
+								/>
+								{/* <h1 className="Product-hero__title">{product.title}</h1> */}
+								<div className="Product-hero__image">
+									{isMobile ? (
+										<img src={product.images[0].src} alt="product" />
+									) : (
+										<div>
+											<ProductOverlay product={product} />
+											<TextureDisplacement
+												image={product.images[0].src}
+												handle={product.handle}
+												size="default"
+											/>
+										</div>
+									)}
 
-                  <div style={{ height: "36px" }} />
-                </div>
-                <div className="Product-hero__scroll">
-                  <span className="Product-hero__discover">Discover</span>
-                  <div className="Product-hero__line" />
-                </div>
-              </div>
-            </div>
-          ) : (
-            ""
-          )}
-          {product.hasOwnProperty("images") ? (
-            <div
-              className="Product-info"
-              dangerouslySetInnerHTML={productMarkup}
-            />
-          ) : (
-            ""
-          )}
-          {product.hasOwnProperty("images") ? (
-            <div className="Product-gallery">
-              {isMobile ? (
-                <img src={product.images[0].src} alt="product" />
-              ) : (
-                <img src={product.images[1].src} alt="product" />
-              )}
-            </div>
-          ) : (
-            ""
-          )}
-          {product.hasOwnProperty("images") ? (
-            <div className="Product-checkout">
-              <div className="Product-checkout__wrapper">
-                <div className="Product-checkout__image">
-                  <img src={product.images[1].src} alt="product" />
-                </div>
-                <div className="Product-checkout__info">
-                  <h1 className="Product-checkout__title">{product.title}</h1>
-                  <div
-                    className="Product-checkout__params"
-                    dangerouslySetInnerHTML={productMarkup}
-                  />
-                  {product.availableForSale ? (
-                    <button
-                      className="Product-checkout__buyButton"
-                      onClick={() => {
-                        this.props.addToCart(
-                          product.variants[0].id,
-                          1,
-                          checkout.id
-                        );
-                        this.props.toggleCart();
-                      }}
-                    >
-                      <span>Add to Cart</span>
-                      <span className="dollar">$</span>
-                      <span>{productPrice}</span>
-                    </button>
-                  ) : (
-                    <button className="Product-checkout__buyButton" disabled>
-                      <span>Sold out</span>
-                    </button>
-                  )}
+									<div style={{ height: "36px" }} />
+								</div>
+								<div className="Product-hero__scroll">
+									<span className="Product-hero__discover">Discover</span>
+									<div className="Product-hero__line" />
+								</div>
+							</div>
+						</div>
+					) : (
+						""
+					)}
+					{product.hasOwnProperty("images") ? (
+						<div
+							className="Product-info"
+							dangerouslySetInnerHTML={productMarkup}
+						/>
+					) : (
+						""
+					)}
+					{product.hasOwnProperty("images") ? (
+						<div className="Product-gallery">
+							{isMobile ? (
+								<img src={product.images[0].src} alt="product" />
+							) : (
+								<img src={product.images[1].src} alt="product" />
+							)}
+						</div>
+					) : (
+						""
+					)}
+					{product.hasOwnProperty("images") ? (
+						<div className="Product-checkout">
+							<div className="Product-checkout__wrapper">
+								<div className="Product-checkout__image">
+									<img src={product.images[1].src} alt="product" />
+								</div>
+								<div className="Product-checkout__info">
+									<h1 className="Product-checkout__title">{product.title}</h1>
+									<div
+										className="Product-checkout__params"
+										dangerouslySetInnerHTML={productMarkup}
+									/>
+									{product.availableForSale ? (
+										<button
+											className="Product-checkout__buyButton"
+											onClick={() => {
+												this.props.addToCart(
+													product.variants[0].id,
+													1,
+													checkout.id
+												);
+												this.props.toggleCart();
+											}}
+										>
+											<span>Add to Cart</span>
+											<span className="dollar">$</span>
+											<span>{productPrice}</span>
+										</button>
+									) : (
+										<button className="Product-checkout__buyButton" disabled>
+											<span>Sold out</span>
+										</button>
+									)}
 
-                  {isBrowser ? (
-                    <Link className="Product-checkout__backButton" to="/works">
-                      Back to Products
-                    </Link>
-                  ) : (
-                    ""
-                  )}
-                </div>
-              </div>
-            </div>
-          ) : (
-            ""
-          )}
-        </div>
-      </div>
-    );
-  }
+									{isBrowser ? (
+										<Link className="Product-checkout__backButton" to="/works">
+											Back to Products
+										</Link>
+									) : (
+										""
+									)}
+								</div>
+							</div>
+						</div>
+					) : (
+						""
+					)}
+				</div>
+			</div>
+		);
+	}
 }
 
 function mapStateToProps(state) {
-  return {
-    product: state.product,
-    checkout: state.checkout
-  };
+	return {
+		product: state.product,
+		checkout: state.checkout
+	};
 }
 
 export default connect(
-  mapStateToProps,
-  { loadProduct, unloadProduct, addToCart, toggleCart }
+	mapStateToProps,
+	{ loadProduct, unloadProduct, addToCart, toggleCart }
 )(withRouter(ProductPage));
